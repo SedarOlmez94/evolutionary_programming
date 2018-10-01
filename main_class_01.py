871f7422610605bf54f57789f96b772fd62d780f
DS\gysol <gysol@leeds.ac.uk>
2018-10-01 15:54:03 +0100

    Need to fix, the newly created tree won't be executed.

--- main_class_01.py
+++ main_class_01.py
@@ -4,23 +4,35 @@ from random import randint, random, choice
 from copy import deepcopy
 from math import log
 
-addw = tree_01.fwrapper(lambda l:l[0]+1[1], 2, 'add')
-subw = tree_01.fwrapper(lambda l:l[0]-1[1], 2, 'subtract')
-mulw = tree_01.fwrapper(lambda l:l[0]*1[1], 2, 'multiply')
+# addw = tree_01.fwrapper(lambda l:l[0]+1[1], 2, 'add')
+# subw = tree_01.fwrapper(lambda l:l[0]-1[1], 2, 'subtract')
+# mulw = tree_01.fwrapper(lambda l:l[0]*1[1], 2, 'multiply')
 
-def iffunc(l):
-    if l[0] > 0:
-        return l[1]
-    else:
-        return l[2]
-ifw = tree_01.fwrapper(iffunc, 3, 'if')
+# # step 1: We create a function
+# def iffunc(l):
+#     if l[0] > 0:
+#         return l[1]
+#     else:
+#         return l[2]
+# # step 2: We initialise an object fwrapper which takes the function, the number of arguments and the name
+# ifw = tree_01.fwrapper(iffunc, 3, 'if')
+#
+# def isgreater(l):
+#     if l[0] > l[1]:
+#         return 1
+#     else:
+#         return 0
+# gtw = tree_01.fwrapper(isgreater, 2, 'isgreater')
 
-def isgreater(l):
-    if l[0] > l[1]:
-        return 1
-    else:
-        return 0
-gtw = tree_01.fwrapper(isgreater, 2, 'isgreater')
 
 #List of functions
-flist = [addw, subw, mulw, ifw, gtw]
+# flist = [addw, subw, mulw, ifw, gtw]
+
+exampletree = tree_01.exampletree
+exampletree.evaluate([2, 3])
+exampletree.evaluate([5, 3])
+
+
+
+# print(exampletree.evaluate([2,3]))
+# print(exampletree.evaluate([5, 3]))
--- tree_01.py
+++ tree_01.py
@@ -35,3 +35,39 @@ class constnode:
         self.v = v
     def evaluate(self, inp):
         return self.v
+
+addw = fwrapper(lambda l:l[0]+l[1], 2, 'add')
+subw = fwrapper(lambda l:l[0]-l[1], 2, 'subtract')
+mulw = fwrapper(lambda l:l[0]*l[1], 2, 'multiply')
+
+# step 1: We create a function
+def iffunc(l):
+    if l[0] > 0:
+        return l[1]
+    else:
+        return l[2]
+
+# step 2: We initialise an object fwrapper which takes the function, the number of arguments and the name
+ifw = fwrapper(iffunc, 3, 'if')
+
+def isgreater(l):
+    if l[0] > l[1]:
+        return 1
+    else:
+        return 0
+gtw = fwrapper(isgreater, 2, 'isgreater')
+
+# step 3: We create a new function example, and return a node with the if function as parent
+def exampletree():
+    #If has 3 arguments, i.e. 3 child nodes which are themselves nodes
+    return node(ifw, [
+#The first if case we check to see if a value is greater than the constant 3
+                        node(gtw, [paramnode(0), constnode(3)]),
+#If the above statement is true, then we add 5 to the value
+                        node(addw [paramnode(1), constnode(5)]),
+#Else we subtract 2 from the value
+                        node(subw [paramnode(2, constnode(2))]),
+                    ]
+                )
+
+flist = [addw, subw, mulw, ifw, gtw]
